e8b8d1b275c8a0d8662d0a1e029f86b652a9c3f8
Harish  A S <109975544+HarishSaminathan@users.noreply.github.com>
2025-07-19 14:35:59 +0530

    Update EquTwo.java

--- EquTwo.java
+++ EquTwo.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class EquTwo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int a = sc.nextInt();
         int b = sc.nextInt();
-        String result = (a == b) ? "Numbers are equal" : "Numbers are not equal";
-        System.out.println(result);
+
+        int xor = a ^ b;  
+
+        String[] result = {"Numbers are equal", "Numbers are not equal"};
+       
+        System.out.println(result[(xor >>> 31) | (xor & 1)]);
+
+        sc.close();
         
     }
 }
